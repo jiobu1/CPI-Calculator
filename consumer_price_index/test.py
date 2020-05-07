2db8fb880e740b246b4001434119d09f2d5d3c70
Jisha Obukwelu <jisha@obukwelu.com>
2020-05-07 13:48:58 -0400

    add init file

--- consumer_price_index/test.py
+++ consumer_price_index/test.py
@@ -1,5 +1,6 @@
 # test.py 
 
-from calculator import inflation_adjustment
+# from calculator import inflation_adjustment
+from consumer_price_index.calculator import inflation_adjustment
 
 print(inflation_adjustment)
