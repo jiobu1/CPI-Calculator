777fa8542ce92c8de70a0d67dd87894ee4870abd
Jisha Obukwelu <jisha@obukwelu.com>
2020-05-10 21:24:07 -0400

    Update calculator class

--- consumer_price_index/class_test.py
+++ consumer_price_index/class_test.py
@@ -1,12 +1,3 @@
 #class_test.py
 
-from consumer_price_index.calculator_class import Reference_Year
-
-amount = int(input("Please choose a number (e.g. 10540) "))
-baseline_year = int(input("[Baseline] Choose a year between 1992 and 2019 "))
-reference_year = int(input("[Reference] Choose a year between 1992 and 2019 "))
-
-baseline_year_instance = Year(baseline_year)
-reference_year_instance = Reference_Year(reference_year, amount)
-dollars = reference_year_instance.get_dollars(baseline_year_instance)
-print(dollars)
+from calculator_class import *
--- consumer_price_index/test.py
+++ consumer_price_index/test.py
@@ -1,7 +1,5 @@
 # test.py 
 
-from consumer_price_index.calculator import inflation_adjustment
+from calculator import inflation_adjustment
 
 print(inflation_adjustment)
-
-
